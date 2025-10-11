e9486527edca95c24f6e09e2c64fa038e0d9c59c
Michael-Hasan <abdukhodirovhasan2002@gmail.com>
2025-10-12 00:06:45 +0900

    feat: Environmental variablelar, Express & Task-H added

--- src/train.ts
+++ src/train.ts
@@ -1,16 +1,31 @@
-function getHighestIndex(arr: number[]): number {
-  if (!Array.isArray(arr) || arr.length === 0) return -1;
+//** TASK-G **//
 
-  let maxVal = arr[0];
-  let maxIdx = 0;
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > maxVal) {
-      maxVal = arr[i];
-      maxIdx = i;
-    }
-  }
-
-  return maxIdx;
+function getPositive(arr: number[]): string {
+  const positiveNums = arr.filter((num) => num > 0);
+  return positiveNums.join("");
 }
-console.log(getHighestIndex([5, 21, 12, 21, 8]));
+
+console.log(getPositive([18, -22, 29]));
+
+//================================================================//
+
+//** TASK-G **//
+
+// function getHighestIndex(arr: number[]): number {
+//   if (!Array.isArray(arr) || arr.length === 0) return -1;
+
+//   let maxVal = arr[0];
+//   let maxIdx = 0;
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > maxVal) {
+//       maxVal = arr[i];
+//       maxIdx = i;
+//     }
+//   }
+
+//   return maxIdx;
+// }
+// console.log(getHighestIndex([5, 21, 12, 21, 8]));
+
+//================================================================//
