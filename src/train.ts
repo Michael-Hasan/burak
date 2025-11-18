cdb3004c932fa5cf4aacccb072937856ec240ba6
Michael-Hasan <abdukhodirovhasan2002@gmail.com>
2025-11-18 15:51:45 +0900

    feat: Task-U added

--- src/train.ts
+++ src/train.ts
@@ -1,9 +1,21 @@
-//** TASK-T **//
-function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
-  return [...arr1, ...arr2].sort((a, b) => a - b);
+//** TASK-U **//
+
+function sumOdds(n: number): number {
+  return Math.floor(n / 2);
 }
 
-console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+console.log(sumOdds(9));
+console.log(sumOdds(11));
+
+//================================================================//
+
+//** TASK-T **//
+
+// function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+//   return [...arr1, ...arr2].sort((a, b) => a - b);
+// }
+
+// console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
 
 //================================================================//
 
